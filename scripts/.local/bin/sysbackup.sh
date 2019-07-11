156b55f0d27ede9ea11c30cd646740f76785b1c3
Marco Wang <m.aesophor@gmail.com>
2019-07-11 22:52:25 +0800

    update indent level

--- scripts/.local/bin/sysbackup.sh
+++ scripts/.local/bin/sysbackup.sh
@@ -13,15 +13,15 @@ backup_file="$backup_dest/$host-$distro-$type-$date.tar.gz"
 
 # Check if the destination exists.
 if [ ! -d $backup_dest ]; then
-    echo "The backup destination is unreachable, aborting..."
-    exit
+  echo "The backup destination is unreachable, aborting..."
+  exit
 fi
 
 # Ask user for confirmation.
 echo "Performing full system backup (/ partition only)"
 echo ""
 echo "Destination: $backup_file"
-echo "/dev, /mnt,/proc, /sys, /tmp, /lost+found and /home will be excluded from this backup."
+echo "/dev, /mnt, /proc, /sys, /tmp, /lost+found and /home will be excluded from this backup."
 echo ""
 echo "To restore from the backup later, boot into a LiveCD, mount the disk on /mnt/mountpoint,"
 echo "cd into the mountpoint and run: tar --xattrs -xvzpf $backup_file -C /mnt/mountpoint"
@@ -31,5 +31,5 @@ read executeback
 
 # Execute backup.
 if [ $executeback == "y" ]; then
-    sudo tar --xattrs -cvzpf $backup_file --exclude=$backup_file --exclude=/boot/EFI --exclude=/dev --exclude=/mnt --exclude=/proc --exclude=/sys --exclude=/tmp --exclude=/lost+found --exclude=/home /
+  sudo tar --xattrs -cvzpf $backup_file --exclude=$backup_file --exclude=/boot/EFI --exclude=/dev --exclude=/mnt --exclude=/proc --exclude=/sys --exclude=/tmp --exclude=/lost+found --exclude=/home /
 fi
