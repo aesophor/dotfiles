41a1dd25885fbdef0baf2117512aec8087404df8
Marco Wang <m.aesophor@gmail.com>
2020-07-21 09:11:42 +0800

    update installer script

--- .install.sh
+++ .install.sh
@@ -3,11 +3,16 @@
 ohmyzsh_install_dest="~/.config/oh-my-zsh"
 vundle_install_dest="~/.vim/bundle/Vundle.vim"
 
+displayctl_clone_dest="~/Code/displayctl"
+py_todo_clone_dest="~/Code/py-todo"
+
+
 # Use GNU Stow to install dotfiles
 echo "[*] Symlinking dotfiles ..."
 mv README.md .README.md
 stow *
 mv .README.md README.md
+
 
 # Install ohmyzsh
 if [ ! -d $ohmyzsh_install_dest ]; then
@@ -20,17 +25,25 @@ else
   echo "[!] $ohmyzsh_install_dest already exists, skipping..."
 fi
 
+
 # Prepare vim/neovim with vundle
 echo "[*] Preparing vim/neovim with vundle ..."
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-mkdir ~/.vim/backups
+mkdir -p ~/.vim/backups
 ~/.vim/bundle/YouCompleteMe/install.py --clang-completer
 echo "[*] Done. Open vim and run the command :PluginInstall"
+
 
 # Install my personal tools
 echo "[*] Installing displayctl and py-todo ..."
 mkdir -p ~/Code
-git clone https://github.com/aesophor/displayctl.git ~/Code/displayctl
-git clone https://github.com/aesophor/py-todo.git ~/Code/py-todo
-sudo cp -a ~/Code/displayctl/displayctl /usr/local/bin/displayctl
-sudo cp -a ~/Code/py-todo/todo /usr/local/bin/todo
+
+if [ ! -d $displayctl_clone_dest ]; then
+  git clone https://github.com/aesophor/displayctl.git "$displayctl_clone_dest"
+  sudo cp -a "$displayctl_clone_dest/displayctl" /usr/local/bin/displayctl
+fi
+
+if [ ! -d ~/Code/py-todo ]; then
+  git clone https://github.com/aesophor/py-todo.git "$py_todo_clone_dest"
+  sudo cp -a "$py_todo_clone_dest/todo" /usr/local/bin/todo
+fi
